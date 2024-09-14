91348ab59260abb2767a1bc316989038174412bb
Scar-46 <87331085+Scar-46@users.noreply.github.com>
2024-09-14 11:28:29 -0600

    Create Lexer.py
    Basic tokens added, this is the base code to start working,

---
+++ PythonLexer/Lexer.py
@@ -0,0 +1,30 @@
+import ply.lex as lex
+
+# List of token names.
+tokens = (
+    'KEYWORD',
+    'IDENTIFIER',
+    'L_NUMERICAL',
+    'L_STRING',
+    'L_BOOLEAN',
+    'O_ARITHMETIC',
+    'O_RELATIONAL',
+    'O_LOGICAL',
+    'O_ASSIGNMENT',
+    'O_OTHER',
+    'D_PARENTHESES',
+    'D_BRACKETS',
+    'D_BRACES',
+    'D_COLON',
+    'D_COMMA',
+    'D_DOT',
+    'D_AT_SIGN',
+    'C_ONE_LINE_COMMENT',
+    'WHITESPACE',
+    'TAB',
+    'NEWLINE'
+    'IDENT',
+    'DEDENT',
+)
+
+
