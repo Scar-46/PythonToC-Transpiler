3f47b12d465dc4ec5c9a6d341817aeacac8193ef
DWAYNE JAMAL TAYLOR MONTERROSA <dwayne.taylor@ucr.ac.cr>
2024-10-20 23:57:24 -0600

    Fixed some minor bugs

--- PythonLexerParser/Tests/test_parser.py
+++ PythonLexerParser/Tests/test_parser.py
@@ -65,6 +65,6 @@ def test_target_list_in_for(parser):
 
 # Test case for reading from a file
 def test_read_file(parser):
-    code = read_file("Tests/sample.py")
+    code = read_file("PythonLexerParser/Tests/sample.py")
     result = parse_code(parser, code)
     assert result is None
