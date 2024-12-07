99429d5c44b528375f7093e8c5c0a4a5a0dc52a4
Javier Alfredo Solano Saltachín <javier.solanosaltachin@ucr.ac.cr>
2024-12-06 22:11:50 -0600

    Fixed missing Call-able methods

--- Compiler/Util/src/Collections/List.hpp
+++ Compiler/Util/src/Collections/List.hpp
@@ -27,9 +27,12 @@ class List : public Object {
   }
 
   void init() override {
-    _methods["has"] = std::bind(&List::has, this, std::placeholders::_1);
     _methods["append"] = std::bind(&List::append, this, std::placeholders::_1);
+    _methods["insert"] = std::bind(&List::insert, this, std::placeholders::_1);
+    _methods["clear"] = std::bind(&List::clear, this, std::placeholders::_1);
+    _methods["has"] = std::bind(&List::has, this, std::placeholders::_1);
     _methods["slice"] = std::bind(&List::slice, this, std::placeholders::_1);
+    _methods["size"] = std::bind(&List::size, this, std::placeholders::_1);
   }
 
  public:
@@ -44,9 +47,35 @@ class List : public Object {
   List(std::initializer_list<var> initList) : elements(initList) { init(); }
   explicit List(std::vector<var> elements) : elements(elements) { init(); }
 
-  // ------------------ Overrides -----------------
+  // ------------------ Native overrides -----------------
 
-  // Override the add method to handle list addition
+  // Print list contents
+  void print(std::ostream& os) const override {
+    os << "[";
+    for (size_t i = 0; i < elements.size(); ++i) {
+      os << elements[i];
+      if (i < elements.size() - 1) {
+        os << ", ";
+      }
+    }
+    os << "]";
+  }
+
+  // Clone self
+  ObjectPtr clone() const override {
+    return std::make_shared<List>(*this);
+  }
+
+  // ------------------ Native operators ------------------
+
+  // Concatenate iself and another list
+  List operator+(const List& other) const {
+    List result = *this;  // Start with a copy of the current list
+    result.elements.insert(result.elements.end(), other.elements.begin(), other.elements.end());
+    return result;
+  }
+
+  // Add itself and another list
   ObjectPtr add(const Object& other) const override {
     auto otherList = dynamic_cast<const List*>(&other);
      if (!otherList) {
@@ -55,7 +84,8 @@ class List : public Object {
     List result = *this + *otherList;
     return std::make_shared<List>(result);
   }
-  // Override the subscript method to indexation
+
+  // Index-access element in list
   ObjectPtr subscript(const Object& other) const override {
     // Attempt to cast the 'other' object to an Integer
     auto otherObj = dynamic_cast<const Integer*>(&other);
@@ -70,7 +100,7 @@ class List : public Object {
     }
   }
 
-  // Override the equals method to compare the lists
+  // Compare contents of self with another list
   bool equals(const Object& other) const override {
     auto otherList = dynamic_cast<const List*>(&other);
     if (!otherList) {
@@ -79,63 +109,101 @@ class List : public Object {
     return elements == otherList->elements;
   }
 
-  // Override print to display list contents
-  void print(std::ostream& os) const override {
-    os << "[";
-    for (size_t i = 0; i < elements.size(); ++i) {
-      os << elements[i];
-      if (i < elements.size() - 1) {
-        os << ", ";
+  // ------------------ Management methods ------------------
+
+  // Add element to end of list
+  Method::result_type append(const std::vector<ObjectPtr>& params) {
+    if (params.size() != 1) {
+      throw std::runtime_error("append: Invalid number of arguments");
+    }
+
+    ObjectPtr element = params[0];
+    if (element) elements.push_back(element->clone());
+
+    return nullptr;
+  }
+
+  // Remove all elements in list
+  Method::result_type clear(const std::vector<ObjectPtr>& params) {
+    if (params.size() != 0) {
+      throw std::runtime_error("clear: Invalid number of arguments");
+    }
+
+    elements.clear();
+    return nullptr;
+  }
+
+  // Insert element on given index
+  Method::result_type insert(const std::vector<ObjectPtr>& params) {
+    if (params.size() != 2) {
+      throw std::runtime_error("insert: Invalid number of arguments");
+    }
+
+    if (! params[0].get()) { 
+      std::cerr << "insert: Invalid null index"; 
+      return nullptr;
+    }
+
+    const Integer* pos = dynamic_cast<const Integer*>(params[0].get());
+
+    if (! pos ) {
+      std::cerr << "insert: Non-integer index"; 
+      return nullptr;
+    }
+
+    var element = params[1];
+
+    if (pos->getValue() < 0 || pos->getValue() > elements.size()) {
+      std::cerr << "Position out of bounds\n";
+      return nullptr;
+    }
+
+    elements.insert(pos->getValue() + elements.begin(), element);
+    return nullptr;
+  }
+
+  // Get amount of elements
+  Method::result_type size(const std::vector<ObjectPtr>& params) {
+    if (params.size() != 0) {
+      throw std::runtime_error("size: Invalid number of arguments");
+    }
+
+    return std::make_shared<Integer>(elements.size());
+  }
+
+  // Scan for element
+  Method::result_type has(const std::vector<ObjectPtr>& params) {
+    if (params.size() != 1) {
+      throw std::runtime_error("has: Invalid number of arguments");
+    }
+
+    ObjectPtr queryPtr = params[0];
+    if (!queryPtr) return std::make_shared<Boolean>(false);
+
+    auto query = var(queryPtr);
+    for (const auto& element : elements) {
+      if (element == query) {
+        return std::make_shared<Boolean>(true);
       }
     }
-    os << "]";
+
+    return std::make_shared<Boolean>(false);
   }
 
-  // Override clone to copy the list
-  ObjectPtr clone() const override {
-    return std::make_shared<List>(*this);
-  }
-
-  // ------------------ List Methods ------------------
-  void addElement(const var& element) {
-    elements.push_back(element);
-  }
-
-  var popElement(size_t index) {
-    if (index >= elements.size()) {
-      std::cerr << "Index out of bounds\n";
-      return var();
-    }
-    var removedElement = elements[index];
-    elements.erase(elements.begin() + index);
-    return removedElement;
-  }
-
-  void clear() {
-    elements.clear();
-  }
-
-  void insert(int pos, const var& element) {
-    if (pos < 0 || static_cast<size_t>(pos) > elements.size()) {
-      std::cerr << "Position out of bounds\n";
-      return;   // Or throw an exception
-    }
-    elements.insert(elements.begin() + pos, element);
-  }
-
-  size_t size() const {
-    return elements.size();
-  }
-
-  // ------------------ Operator Overloading ------------------
-  // Overload the + operator to concatenate two lists
-  List operator+(const List& other) const {
-    List result = *this;  // Start with a copy of the current list
-    result.elements.insert(result.elements.end(), other.elements.begin(), other.elements.end());
-    return result;
+  // Return sliced list
+  Method::result_type slice(const std::vector<ObjectPtr>& params) {
+    return generalizedSlice(
+      this->elements,
+      params,
+      [](std::vector<var>& result, const var& element) { result.push_back(element); },
+      [](const std::vector<var>& resultContainer) {
+        return std::make_shared<List>(resultContainer);
+      }
+    );    // NOLINT
   }
 
   // ------------------ Iterator ------------------
+
   class ListIterator : public Object::ObjectIterator {
    private:
     const List& _list;
@@ -145,7 +213,7 @@ class List : public Object {
     explicit ListIterator(const List& list) : _list(list), _currentIndex(0) {}
 
     bool hasNext() const override {
-      return _currentIndex < _list.size();
+      return _currentIndex < _list.elements.size();
     }
 
     ObjectPtr next() override {
@@ -163,40 +231,5 @@ class List : public Object {
   // Override iteration methods
   ObjectIt getIterator() const override {
     return std::make_unique<ListIterator>(*this);
-  }
-
-  // ------------------ List Methods for Bind ------------------
-  Method::result_type has(const std::vector<ObjectPtr>& params) {
-    Boolean result = Boolean(false);
-    if (params.size() <= 0) return std::make_shared<Boolean>(result);
-    auto queryPtr = *(params.begin());
-    if (!queryPtr) return std::make_shared<Boolean>(result);
-    auto query = var(queryPtr);
-    for (const auto& element : elements) {
-      if (element == query) {
-        result = Boolean(true);
-      }
-    }
-
-    return std::make_shared<Boolean>(result);
-  }
-
-  Method::result_type append(const std::vector<ObjectPtr>& params) {
-    if (params.size() <= 0) return nullptr;
-    ObjectPtr element = *(params.begin());
-    if (element) elements.push_back(var(element->clone()));
-
-    return nullptr;
-  }
-
-  Method::result_type slice(const std::vector<ObjectPtr>& params) {
-    return generalizedSlice(
-      this->elements,
-      params,
-      [](std::vector<var>& result, const var& element) { result.push_back(element); },
-      [](const std::vector<var>& resultContainer) {
-        return std::make_shared<List>(resultContainer);
-      }
-    );    // NOLINT
   }
 };
