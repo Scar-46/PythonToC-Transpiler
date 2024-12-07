171e21a410da793fe6b188c86906588d0f7e3b1b
Javier Alfredo Solano Saltachín <javier.solanosaltachin@ucr.ac.cr>
2024-12-07 13:47:40 -0600

    Split integer into .cpp and .hpp

--- Compiler/Util/src/Numeric/Integer.hpp
+++ Compiler/Util/src/Numeric/Integer.hpp
@@ -1,74 +1,27 @@
 // Copyright (c) 2024 Syntax Errors.
 #pragma once
 
-#include <memory>
-
 #include "./Numeric.hpp"
-#include "./Double.hpp"
 
 // Integer class
 // TODO(Dwayne): Implement like division.
 class Integer : public BaseNumeric<Integer, int32_t> {
- public:
-  explicit Integer(int32_t value) : BaseNumeric(value) {}
+	public:
+		explicit Integer(int32_t value);
 
-  operator ObjectPtr() override{
-    return std::make_shared<Integer>(*this);
-  };
+		operator ObjectPtr() override;
 
-  ObjectPtr addHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return std::make_shared<Integer>(value + otherObj->getValue());
-    }
-    throw std::runtime_error("Integer does not support addition with given type");
-  }
+		ObjectPtr addHelper(const Object& other) const override;
 
-  ObjectPtr subtractHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return std::make_shared<Integer>(value - otherObj->getValue());
-    }
-    throw std::runtime_error("Integer does not support subtraction with given type");
-  }
+		ObjectPtr subtractHelper(const Object& other) const override;
 
-  ObjectPtr multiplyHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return std::make_shared<Integer>(value + otherObj->getValue());
-    }
-    throw std::runtime_error("Integer does not support multiplication with given type");
-  }
+		ObjectPtr multiplyHelper(const Object& other) const override;
 
-  ObjectPtr divideHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return std::make_shared<Integer>(value + otherObj->getValue());
-    }
-    throw std::runtime_error("Integer does not support division with given type");
-  }
+		ObjectPtr divideHelper(const Object& other) const override;
 
-  bool equalsHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return value == otherObj->getValue();
-    }
-    return false;
-  }
+		bool equalsHelper(const Object& other) const override;
 
-  bool lessHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return value < otherObj->getValue();
-    }
-    return false;
-  }
+		bool lessHelper(const Object& other) const override;
 
-  bool greaterHelper(const Object& other) const override {
-    auto otherObj = dynamic_cast<const Double*>(&other);
-    if (otherObj) {
-      return value > otherObj->getValue();
-    }
-    return false;
-  }
+		bool greaterHelper(const Object& other) const override;
 };
